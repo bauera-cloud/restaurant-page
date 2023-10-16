71493abcf29b324d431dfaab97eaab73c15102d4
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-10-16 13:49:26 -0400

    Create logic for links to change html of page
    Though the 'MENU' page hasn't been created yet, the 'HOME' and
    'ABOUT' pages will determine what goes in the html for the page
    depending on which link the user clicked.

--- src/index.js
+++ src/index.js
@@ -6,28 +6,22 @@ import './style.css';
 let contentDiv = document.createElement("div");
 contentDiv.setAttribute('id', 'content');
 
-
 contentDiv.appendChild(navbar)
-contentDiv.appendChild(aboutContent)
+contentDiv.appendChild(homepageContent)
 contentDiv.appendChild(footer)
 
 document.body.appendChild(contentDiv)
 
-// contentDiv.appendChild(homepageContent)
+//make the logic for changing pages
+let navNodes = document.querySelectorAll('#navbar>ul>li>a');
 
-// let navbarLinks = document.querySelector('nav>ul');
-// // navbarLinks.addEventListener('click', userClicksNavLink)
-
-// console.log(homepageContent)
-
-// //get first child of content
-// function userClicksNavLink(e) {
-//     let link = e.target.id;
-//     // if (link === 'about') {
-//     //     contentDiv.removeChild(contentDiv.lastChild)
-//     //     contentDiv.appendChild(aboutContent)
-//     // } else if (link === 'home') {
-//     //     contentDiv.removeChild(contentDiv.lastChild)
-//     //     contentDiv.appendChild(homepageContent)
-//     // }
-// }
+navNodes.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        let mainContent = document.querySelector('.main-content');
+        if (link.id === 'about') {
+            mainContent.replaceWith(aboutContent)
+        } else if (link.id === 'home') {
+            mainContent.replaceWith(homepageContent)
+        }
+    })
+})
